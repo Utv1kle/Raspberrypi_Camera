4e9295158074f8e0394fa149f096adc7cded6de1
Utv1kle <123aaq@naver.com>
2016-03-07 18:23:26 -0800

    1

---
+++ camera1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import os,sys
+
+def First():
+	f = open("setting.txt", 'w')
+	f.close()
+	#win.fullscreen()
+
+class Button(Gtk.Window):
+
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Button Demo")
+		self.set_border_width(10)
+
+		hbox = Gtk.Box(spacing=6)
+		self.add(hbox)
+
+		button = Gtk.Button.new_with_label("Save")
+		button.connect("clicked", self.Save)
+		hbox.pack_start(button, True, True, 0)
+
+		button = Gtk.Button.new_with_mnemonic("Cancel")
+		button.connect("clicked", self.Cancel)
+		hbox.pack_start(button, True, True, 0)
+
+	def Save(self, button):
+		print("\"Click me\" button was clicked")
+	
+	def Cancel(self, button):
+		print("CanCel")
+		Gtk.main_quit()
+First()
+win = Button()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
